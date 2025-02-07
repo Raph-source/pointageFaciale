f544cd537038f76330f38bef4a806c704622eebb
raph <20ik061@esisalama.org>
2025-02-07 19:18:21 +0200

    séparation front et back

---
+++ backend/pointeur facial/coupeurVisage.py
@@ -0,0 +1,26 @@
+from deepface import DeepFace
+import os
+from tqdm import tqdm
+import cv2
+import matplotlib.pyplot as plt
+
+def rogner(dossier_entree, dossier_sortie, detector_backend="yolov8"):
+    #créer le dossier de sortie si il n'existe pas
+    os.makedirs(dossier_sortie, exist_ok=True)
+
+    for image in tqdm(os.listdir(dossier_entree)):
+        chemin_image = os.path.join(dossier_entree, image)
+        nom_image = image.split(".")[0]
+
+        face = DeepFace.extract_faces(
+            chemin_image,
+            detector_backend=detector_backend,
+            enforce_detection=True,
+            Target_size=None,
+        )[0]["face"]
+
+        face = cv2.cvtColor(face, cv2.COLOR_GRAY2RGB)
+        face = cv2.resize(face, (224, 224))
+        plt.imsave(f"{dossier_sortie}/{nom_image}.jpg", face)
+
+rogner("../uploads/", "./vigage")
