822c4adcf1e7037bd4d3529c481fff4aa16f5207
raph <20ik061@esisalama.org>
2025-02-08 09:30:58 +0200

    couper les visages

--- backend/pointeur facial/coupeurVisage.py
+++ backend/pointeur facial/coupeurVisage.py
@@ -3,6 +3,7 @@ import os
 from tqdm import tqdm
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 def rogner(dossier_entree, dossier_sortie, detector_backend="yolov8"):
     #créer le dossier de sortie si il n'existe pas
@@ -16,8 +17,12 @@ def rogner(dossier_entree, dossier_sortie, detector_backend="yolov8"):
             chemin_image,
             detector_backend=detector_backend,
             enforce_detection=True,
-            Target_size=None,
+            grayscale=True,
         )[0]["face"]
+
+        # Assurez-vous que l'image est dans un format compatible
+        if face.dtype != np.uint8:
+            face = (255 * face).astype(np.uint8)  # Normalisez et convertissez en uint8
 
         face = cv2.cvtColor(face, cv2.COLOR_GRAY2RGB)
         face = cv2.resize(face, (224, 224))
